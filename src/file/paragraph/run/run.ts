bf9a1c67507534c0e814a5f8ef829cd80fbb716e
wangfengming <wangfengming@hexin.im>
2020-07-11 16:05:15 +0800

    :feat: refine paragraph & run options

--- src/file/paragraph/run/run.ts
+++ src/file/paragraph/run/run.ts
@@ -1,50 +1,15 @@
 // http://officeopenxml.com/WPtext.php
-import { ShadingType } from "file/table";
 import { XmlComponent } from "file/xml-components";
 
 import { FootnoteReferenceRun } from "file/footnotes/footnote/run/reference-run";
 import { FieldInstruction } from "file/table-of-contents/field-instruction";
 import { Break } from "./break";
-import { EmphasisMarkType } from "./emphasis-mark";
 import { Begin, End, Separate } from "./field";
 import { NumberOfPages, NumberOfPagesSection, Page } from "./page-number";
-import { RunProperties } from "./properties";
+import { IRunPropertiesOptions, RunProperties } from "./properties";
 import { Text } from "./run-components/text";
-import { IFontAttributesProperties } from "./run-fonts";
-import { UnderlineType } from "./underline";
 
-interface IFontOptions {
-    readonly name: string;
-    readonly hint?: string;
-}
-
-export interface IRunOptions {
-    readonly bold?: boolean;
-    readonly italics?: boolean;
-    readonly underline?: {
-        readonly color?: string;
-        readonly type?: UnderlineType;
-    };
-    readonly emphasisMark?: {
-        readonly type?: EmphasisMarkType;
-    };
-    readonly color?: string;
-    readonly size?: number;
-    readonly rightToLeft?: boolean;
-    readonly smallCaps?: boolean;
-    readonly allCaps?: boolean;
-    readonly strike?: boolean;
-    readonly doubleStrike?: boolean;
-    readonly subScript?: boolean;
-    readonly superScript?: boolean;
-    readonly style?: string;
-    readonly font?: string | IFontOptions | IFontAttributesProperties;
-    readonly highlight?: string;
-    readonly shading?: {
-        readonly type: ShadingType;
-        readonly fill: string;
-        readonly color: string;
-    };
+export interface IRunOptions extends IRunPropertiesOptions {
     readonly children?: Array<Begin | FieldInstruction | Separate | End | PageNumber | FootnoteReferenceRun | string>;
     readonly text?: string;
 }
