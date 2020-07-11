b652bd89213f77bc74c910d4bd27637fee57a7a2
wangfengming <wangfengming@hexin.im>
2020-07-11 16:06:07 +0800

    :build: build

--- build/file/paragraph/run/run.d.ts
+++ build/file/paragraph/run/run.d.ts
@@ -1,43 +1,9 @@
-import { ShadingType } from "../../../file/table";
 import { XmlComponent } from "../../../file/xml-components";
 import { FootnoteReferenceRun } from "../../../file/footnotes/footnote/run/reference-run";
 import { FieldInstruction } from "../../../file/table-of-contents/field-instruction";
-import { EmphasisMarkType } from "./emphasis-mark";
 import { Begin, End, Separate } from "./field";
-import { RunProperties } from "./properties";
-import { IFontAttributesProperties } from "./run-fonts";
-import { UnderlineType } from "./underline";
-interface IFontOptions {
-    readonly name: string;
-    readonly hint?: string;
-}
-export interface IRunOptions {
-    readonly bold?: boolean;
-    readonly italics?: boolean;
-    readonly underline?: {
-        readonly color?: string;
-        readonly type?: UnderlineType;
-    };
-    readonly emphasisMark?: {
-        readonly type?: EmphasisMarkType;
-    };
-    readonly color?: string;
-    readonly size?: number;
-    readonly rightToLeft?: boolean;
-    readonly smallCaps?: boolean;
-    readonly allCaps?: boolean;
-    readonly strike?: boolean;
-    readonly doubleStrike?: boolean;
-    readonly subScript?: boolean;
-    readonly superScript?: boolean;
-    readonly style?: string;
-    readonly font?: string | IFontOptions | IFontAttributesProperties;
-    readonly highlight?: string;
-    readonly shading?: {
-        readonly type: ShadingType;
-        readonly fill: string;
-        readonly color: string;
-    };
+import { IRunPropertiesOptions, RunProperties } from "./properties";
+export interface IRunOptions extends IRunPropertiesOptions {
     readonly children?: Array<Begin | FieldInstruction | Separate | End | PageNumber | FootnoteReferenceRun | string>;
     readonly text?: string;
 }
@@ -51,4 +17,3 @@ export declare class Run extends XmlComponent {
     constructor(options: IRunOptions);
     break(): Run;
 }
-export {};
