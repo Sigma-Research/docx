623e4a12216d5555b129857774e967dcf592350c
Hunderhan <hunderhan@163.com>
2022-08-02 17:20:46 +0800

    feat: 删除build 文件夹

--- build/file/styles/defaults/paragraph-properties.d.ts
+++
@@ -1,5 +0,0 @@
-import { IParagraphPropertiesOptions } from "../../../file/paragraph/properties";
-import { XmlComponent } from "../../../file/xml-components";
-export declare class ParagraphPropertiesDefaults extends XmlComponent {
-    constructor(options?: IParagraphPropertiesOptions);
-}
--- build/file/styles/defaults/run-properties.d.ts
+++
@@ -1,9 +0,0 @@
-import { IRunPropertiesOptions } from "../../../file/paragraph/run/properties";
-import { IFontAttributesProperties } from "../../../file/paragraph/run/run-fonts";
-import { XmlComponent } from "../../../file/xml-components";
-export declare class RunPropertiesDefaults extends XmlComponent {
-    private readonly properties;
-    constructor(options?: IRunPropertiesOptions);
-    size(size: number): RunPropertiesDefaults;
-    font(font: string | IFontAttributesProperties): RunPropertiesDefaults;
-}
