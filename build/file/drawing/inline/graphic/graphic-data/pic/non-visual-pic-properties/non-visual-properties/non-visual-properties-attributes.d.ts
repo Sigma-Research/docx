d5a33d932ea5adf50b0bd65ff28af9d0839a0bb6
Hunderhan <hunderhan@163.com>
2022-08-08 14:26:13 +0800

    feat: build

---
+++ build/file/drawing/inline/graphic/graphic-data/pic/non-visual-pic-properties/non-visual-properties/non-visual-properties-attributes.d.ts
@@ -0,0 +1,14 @@
+import { XmlAttributeComponent } from "../../../../../../../../file/xml-components";
+export declare class NonVisualPropertiesAttributes extends XmlAttributeComponent<{
+    readonly id?: number;
+    readonly name?: string;
+    readonly descr?: string;
+    readonly title?: string;
+}> {
+    protected readonly xmlKeys: {
+        id: string;
+        name: string;
+        descr: string;
+        title: string;
+    };
+}
